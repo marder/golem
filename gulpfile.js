0460b9bb7959d20a5b006ecfe9f5962aefaebca5
Mike <mharder@stoehr-sauer.de>
2017-12-07 11:21:31 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp");
 var tsc = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
+var del = require("del");
 
 let tsProject = tsc.createProject("tsconfig.json");
 
 gulp.task("default", ["build"]);
 
-gulp.task("build", function () {
+gulp.task("cleanup", function () {
+	return del(["build", "maps", "declarations"]);
+})
+
+// Transpile ts > js with generating sourcemaps
+gulp.task("build-debug", ["cleanup"], function () {
 	var tsResult = gulp.src("src/**/*.ts")
 		.pipe(sourcemaps.init())
 		.pipe(tsProject());
@@ -14,6 +20,14 @@ gulp.task("build", function () {
 	return tsResult.js.pipe(sourcemaps.write("../maps")).pipe(gulp.dest("build"));
 })
 
+// Transpile ts > js without generating sourcemaps
+gulp.task("build", ["cleanup"], function () {
+	var tsResult = gulp.src("src/**/*.ts")
+		.pipe(tsProject());
+	tsResult.dts.pipe(gulp.dest("declarations"));
+	return tsResult.js.pipe(gulp.dest("build"));
+})
+
 gulp.task("watch", function () {
-	gulp.watch("src/**/*.ts", ["build"]);
+	gulp.watch("src/**/*.ts", ["build-debug"]);
 });
